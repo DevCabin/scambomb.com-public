8f2f842929b1c296b7e740a2024319a048862519
George Featherstone <george@devcabin.com>
2026-03-12 17:33:49 -0500

    pixel id

--- app/protect-parents/page.tsx
+++ app/protect-parents/page.tsx
@@ -3,6 +3,8 @@
 import { useEffect } from 'react'
 import Script from 'next/script'
 
+const FB_PIXEL_ID = '486466700592099'
+
 export default function ProtectParentsPage() {
   useEffect(() => {
     const reveals = document.querySelectorAll('.reveal')
@@ -37,7 +39,7 @@ export default function ProtectParentsPage() {
   return (
     <>
       <Script id="fb-pixel-init" strategy="afterInteractive">
-        {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', 'REPLACE_WITH_YOUR_PIXEL_ID');fbq('track', 'PageView');`}
+        {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '${FB_PIXEL_ID}');fbq('track', 'PageView');`}
       </Script>
 
       <section className="hero">
