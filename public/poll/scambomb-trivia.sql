e37d42043b62059c2b463756f108b7ef05f83065
George Featherstone <george@devcabin.com>
2026-06-23 07:56:56 -0500

    Change trivia event code to TRIVIA-625

--- public/poll/scambomb-trivia.sql
+++ public/poll/scambomb-trivia.sql
@@ -5,11 +5,11 @@
 begin;
 
 delete from events
-where code = 'SCAMBOMB-TRIVIA';
+where code = 'TRIVIA-625';
 
 with new_event as (
   insert into events (code, active_question_id)
-  values ('SCAMBOMB-TRIVIA', null)
+  values ('TRIVIA-625', null)
   returning id
 )
 insert into questions (event_id, prompt, options, sort_order, revealed, answer_revealed, correct_answer, explanation)
@@ -65,6 +65,6 @@ cross join (
 commit;
 
 -- URLs:
--- Vote:     https://scambomb.com/poll/event/SCAMBOMB-TRIVIA
--- Results:  https://scambomb.com/poll/event/SCAMBOMB-TRIVIA/results
--- Control:  https://scambomb.com/poll/event/SCAMBOMB-TRIVIA/control
+-- Vote:     https://scambomb.com/poll/event/TRIVIA-625
+-- Results:  https://scambomb.com/poll/event/TRIVIA-625/results
+-- Control:  https://scambomb.com/poll/event/TRIVIA-625/control
